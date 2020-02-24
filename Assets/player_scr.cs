d673164ea9245ad8938a40ec858b7d4735b40dda
Isak Forsberg <isak-forsberg@hotmail.se>
2020-02-24 19:52:27 +0100

    Player Movement, Temp Sprite, Player Prefab

--- Assets/player_scr.cs
+++ Assets/player_scr.cs
@@ -11,6 +11,9 @@ public class player_scr : MonoBehaviour
     public KeyCode switchChar_k = KeyCode.Z;
     public KeyCode highFive_k = KeyCode.X;
 
+    [Min(0.0f)]
+    public float speed = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +23,13 @@ public class player_scr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(up_k))
+            transform.Translate(0.0f, speed, 0.0f);
+        if (Input.GetKey(down_k))
+            transform.Translate(0.0f, -speed, 0.0f);
+        if (Input.GetKey(left_k))
+            transform.Translate(-speed, 0.0f, 0.0f);
+        if (Input.GetKey(right_k))
+            transform.Translate(speed, 0.0f, 0.0f);
     }
 }
