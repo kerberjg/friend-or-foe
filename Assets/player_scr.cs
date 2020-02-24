3bdd874e97c4dca8d7322a9e5ed0fc546d00718f
Isak Forsberg <isak-forsberg@hotmail.se>
2020-02-24 17:46:12 +0100

    Player script added

---
+++ Assets/player_scr.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_scr : MonoBehaviour
+{
+    public KeyCode up_k = KeyCode.UpArrow;
+    public KeyCode down_k = KeyCode.DownArrow;
+    public KeyCode left_k = KeyCode.LeftArrow;
+    public KeyCode right_k = KeyCode.RightArrow;
+    public KeyCode switchChar_k = KeyCode.Z;
+    public KeyCode highFive_k = KeyCode.X;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
