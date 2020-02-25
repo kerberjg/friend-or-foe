585594910bff60c1be1d980f5e5ffcd1738d6ab7
Isak Forsberg <isak-forsberg@hotmail.se>
2020-02-25 23:21:13 +0100

    Updated Movement

--- Assets/player_scr.cs
+++ Assets/player_scr.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class player_scr : MonoBehaviour
 {
+    enum Character
+    {
+        MOVING,
+        SEEING
+    }
+
     public KeyCode up_k = KeyCode.UpArrow;
     public KeyCode down_k = KeyCode.DownArrow;
     public KeyCode left_k = KeyCode.LeftArrow;
@@ -13,23 +19,89 @@ public class player_scr : MonoBehaviour
 
     [Min(0.0f)]
     public float speed = 1.0f;
+    [Min(0.0f)]
+    public float switchSpeed = 1.0f;
+    [Min(0.0f)]
+    public float moveDelay = 0.5f;
+
+    Character curCharacter = Character.MOVING;
+    Transform[] charactersTrans;
+    float curMoveDelay = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        charactersTrans = GetComponentsInChildren<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(up_k))
-            transform.Translate(0.0f, speed, 0.0f);
-        if (Input.GetKey(down_k))
-            transform.Translate(0.0f, -speed, 0.0f);
-        if (Input.GetKey(left_k))
-            transform.Translate(-speed, 0.0f, 0.0f);
-        if (Input.GetKey(right_k))
-            transform.Translate(speed, 0.0f, 0.0f);
+        if (curCharacter == Character.MOVING)
+        {
+            Vector3 direction = new Vector3(0.0f, 0.0f, 0.0f);
+
+            if (Input.GetKey(up_k))
+            {
+                direction.y += 1.0f;
+            }
+            if (Input.GetKey(down_k))
+            {
+                direction.y -= 1.0f;
+            }
+            if (Input.GetKey(left_k))
+            {
+                direction.x -= 1.0f;
+            }
+            if (Input.GetKey(right_k))
+            {
+                direction.x += 1.0f;
+            }
+            direction.Normalize();
+
+            charactersTrans[0].Translate(direction * speed);
+            charactersTrans[2].Translate(-direction * speed);
+
+            if (charactersTrans[2].position.x < charactersTrans[1].position.x - 0.51f ||
+                charactersTrans[2].position.x > charactersTrans[1].position.x - 0.49f ||
+                charactersTrans[2].position.y < charactersTrans[1].position.y + 0.19f ||
+                charactersTrans[2].position.y > charactersTrans[1].position.y + 0.21f)
+            {
+                curMoveDelay += Time.deltaTime;
+            }
+            else
+            {
+                curMoveDelay = 0.0f;
+            }
+
+            if (curMoveDelay >= moveDelay)
+            {
+                Vector3 direction2 = charactersTrans[1].position;
+                direction2.x -= 0.5f;
+                direction2.y += 0.2f;
+                direction2 -= charactersTrans[2].position;
+
+                if (direction2.magnitude > (direction2.normalized * speed).magnitude)
+                    charactersTrans[2].Translate(direction2.normalized * speed);
+                else
+                    charactersTrans[2].Translate(direction2);
+            }
+        }
+        else if (curCharacter == Character.SEEING)
+        {
+
+        }
+
+        if (Input.GetKeyDown(switchChar_k))
+        {
+            if (curCharacter == Character.MOVING)
+            {
+                curCharacter = Character.SEEING;
+            }
+            else if (curCharacter == Character.SEEING)
+            {
+                curCharacter = Character.MOVING;
+            }
+        }
     }
 }
