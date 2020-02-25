c9b1251272dadfc9ad9814ca4450b36a9991da06
Isak Forsberg <isak-forsberg@hotmail.se>
2020-02-25 21:59:06 +0100

    Sprite Layer Order script

---
+++ Assets/layerOrder_scr.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class layerOrder_scr : MonoBehaviour
+{
+    SpriteRenderer mSpriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mSpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mSpriteRenderer.sortingOrder = Mathf.FloorToInt(-transform.position.y * 1000.0f);
+    }
+}
