a18d0ff03eaf38c17d61d3e8418c923966438e37
James Kerber <kerber.jg@gmail.com>
2020-02-26 16:05:35 +0100

    Created template EnemySpawner script

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Header("Spawn density control")]
+    public int minCount;
+    public int maxCount;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FillSegment(GameObject segment, float difficulty) {
+        // calculate how many entitites to spawn
+        int count = Mathf.RoundToInt( Mathf.Lerp(minCount, maxCount, difficulty) );
+
+        for(int i = 0; i < count; i++) {
+            Vector3 pos = new Vector3(); // calculate random position
+            Spawn(pos, segment);
+        }
+            
+    }
+
+    void Spawn(Vector3 position, GameObject parent) {
+        // spawn code for single entities goes here
+    }
+}
