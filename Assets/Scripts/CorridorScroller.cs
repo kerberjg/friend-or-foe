e23a32f82bfb80518658d4453d0205bf664a02a7
James Kerber <kerber.jg@gmail.com>
2020-02-26 15:57:42 +0100

    Created a basic scrolling room

---
+++ Assets/Scripts/CorridorScroller.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorridorScroller : MonoBehaviour
+{
+    [Header("Corridor elements")]
+    public GameObject[] corridorPrefabs;
+    public GameObject corridorPrev;
+    public GameObject corridorCurrent;
+    public GameObject corridorNext;
+
+    [Header("Scrolling settings")]
+    public bool random = false;
+    public float elementLength = 10;
+    public float scrollSpeed = 2f;
+    public Vector3 scrollDir = Vector3.left;
+    public float scrollPos = 0;
+    public int elementIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        corridorPrev = LoadNext();
+        corridorPrev.transform.position = scrollDir * elementLength;
+
+        corridorCurrent = LoadNext();
+        corridorCurrent.transform.position = scrollDir * 0;
+
+        corridorNext = LoadNext();
+        corridorNext.transform.position = scrollDir * -elementLength;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // scroll
+        float scrollDelta = scrollSpeed * Time.deltaTime;
+        scrollPos = scrollPos + scrollDelta;
+
+        corridorPrev.transform.Translate(scrollDir * scrollDelta);
+        corridorCurrent.transform.Translate(scrollDir * scrollDelta);
+        corridorNext.transform.Translate(scrollDir * scrollDelta);
+
+        // load & unload
+        if(scrollPos >= elementLength) {
+            scrollPos -= elementLength;
+
+            if(corridorPrev != null)
+                Destroy(corridorPrev);
+
+            corridorPrev = corridorCurrent;
+            corridorCurrent = corridorNext;
+            corridorNext = LoadNext();
+        }
+    }
+
+    GameObject LoadNext() {
+        int index;
+        if(random) {
+            index = Random.Range(0, corridorPrefabs.Length);
+        } else {
+            index = elementIndex++ % corridorPrefabs.Length;
+        }
+
+        return Instantiate(corridorPrefabs[index], new Vector3(elementLength, 0f, 0f), Quaternion.identity, this.transform);
+    }
+}
