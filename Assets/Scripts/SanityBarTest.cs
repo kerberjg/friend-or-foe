0aa434e420df4491947e3e229fb856844317dd69
Karam <Karam.hariri@outlook.com>
2020-02-28 17:15:50 +0100

    Sanity Bar with a test script attached to a "Player"

---
+++ Assets/Scripts/SanityBarTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SanityBarTest : MonoBehaviour
+{
+    public Image fill;
+    [Range(0.0f, 100.0f)]
+    public float health = 100.0f;
+    public float maxHealth = 100.0f;
+
+    private void Start()
+    {
+        health = 100.0f;
+        maxHealth = 100.0f;
+    }
+
+    void Update()
+    {
+        fill.fillAmount = health / maxHealth;
+    }
+}
