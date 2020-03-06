99eb8e73393bb33d11d77ac46b2db74b318592a4
Isak Forsberg <isak-forsberg@hotmail.se>
2020-03-06 20:00:08 +0100

    Switch Animation Working

--- Assets/Scripts/player_scr.cs
+++ Assets/Scripts/player_scr.cs
@@ -10,19 +10,6 @@ public class player_scr : MonoBehaviour
         SEEING
     }
 
-    //enum DelayDirection
-    //{
-    //    IDLE,
-    //    UP,
-    //    UP_RIGHT,
-    //    RIGHT,
-    //    DOWN_RIGHT,
-    //    DOWN,
-    //    DOWN_LEFT,
-    //    LEFT,
-    //    UP_LEFT
-    //}
-
 
     public KeyCode up_k = KeyCode.UpArrow;
     public KeyCode down_k = KeyCode.DownArrow;
@@ -35,8 +22,6 @@ public class player_scr : MonoBehaviour
     public float speed = 0.03f;
     [Min(0.0f)]
     public float switchSpeed = 0.03f;
-    //[Min(0.0f)]
-    //public float moveDelay = 0.3f;
     public float xMin = -7.0f;
     public float xMax = -2.0f;
     public float yMin = -2.0f;
@@ -46,275 +31,81 @@ public class player_scr : MonoBehaviour
     public string friendTag = "Friend";
 
     Character curCharacter = Character.MOVING;
-    //Transform[] charactersTrans;
-    //float curMoveDelay = 0.0f;
     bool switchCharacter = false;
-    //List<DelayDirection> delayDirections = new List<DelayDirection>();
+
+    Animator spriteAnimator;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //charactersTrans = GetComponentsInChildren<Transform>();
+        spriteAnimator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (curMoveDelay < moveDelay)
-        //{
-        //    curMoveDelay += Time.deltaTime;
-        //}
-        //else
-        //{
-        //    delayDirections.RemoveAt(0);
-        //}
-
         if (curCharacter == Character.MOVING)
         {
-            //if (switchCharacter)
-            //{
-            //    //delayDirections.Add(DelayDirection.IDLE);
+            if (switchCharacter)
+            {
+                spriteAnimator.SetBool("DoSwitch", false);
 
-            //    //Vector3 switchDirection = -transform.localPosition;
-            //    //Vector3 switchDirection2 = new Vector3(-0.5f, 0.2f, 0.0f) - charactersTrans[2].localPosition;
+                switchCharacter = false;
+            }
 
-            //    Vector3 switchDirection = transform.localPosition;
+            Vector3 moveDirection = new Vector3(0.0f, 0.0f, 0.0f);
 
-            //    if (transform.position.x < -0.01f ||
-            //        transform.position.x > 0.01f ||
-            //        transform.position.y < -0.01f ||
-            //        transform.position.y > 0.01f)
-            //    {
-            //        if (switchDirection.magnitude > (switchDirection.normalized * switchSpeed).magnitude)
-            //            transform.Translate(switchDirection.normalized * switchSpeed);
-            //        else
-            //        {
-            //            transform.Translate(switchDirection);
-            //            switchCharacter = false;
-            //        }
-            //    }
+            if (Input.GetKey(up_k))
+            {
+                moveDirection.y -= 1.0f;
+            }
+            if (Input.GetKey(down_k))
+            {
+                moveDirection.y += 1.0f;
+            }
+            if (Input.GetKey(left_k))
+            {
+                moveDirection.x -= 1.0f;
+            }
+            if (Input.GetKey(right_k))
+            {
+                moveDirection.x += 1.0f;
+            }
+            moveDirection.Normalize();
 
-            //    //if (charactersTrans[2].position.x < -0.51f ||
-            //    //    charactersTrans[2].position.x > -0.49f ||
-            //    //    charactersTrans[2].position.y < 0.19f ||
-            //    //    charactersTrans[2].position.y > 0.21f)
-            //    //{
-            //    //    if (switchDirection2.magnitude > (switchDirection2.normalized * switchSpeed).magnitude)
-            //    //        charactersTrans[2].Translate(switchDirection2.normalized * switchSpeed);
-            //    //    else
-            //    //        charactersTrans[2].Translate(switchDirection2);
-            //    //}
-            //}
-            //else
-            //{
-                //int newDir = 0;
-                //if (Input.GetKey(up_k))
-                //{
-                //    newDir += 1;
-                //}
-                //if (Input.GetKey(down_k))
-                //{
-                //    newDir += 2;
-                //}
-                //if (Input.GetKey(left_k))
-                //{
-                //    newDir += 4;
-                //}
-                //if (Input.GetKey(right_k))
-                //{
-                //    newDir += 8;
-                //}
+            transform.Translate(moveDirection * speed);
 
-                //if (newDir == 1 || newDir == 13)
-                //{
-                //    delayDirections.Add(DelayDirection.UP);
-                //}
-                //else if (newDir == 2 || newDir == 14)
-                //{
-                //    delayDirections.Add(DelayDirection.DOWN);
-                //}
-                //else if (newDir == 4 || newDir == 7)
-                //{
-                //    delayDirections.Add(DelayDirection.LEFT);
-                //}
-                //else if (newDir == 8 || newDir == 11)
-                //{
-                //    delayDirections.Add(DelayDirection.RIGHT);
-                //}
-                //else if (newDir == 5)
-                //{
-                //    delayDirections.Add(DelayDirection.UP_LEFT);
-                //}
-                //else if (newDir == 6)
-                //{
-                //    delayDirections.Add(DelayDirection.DOWN_LEFT);
-                //}
-                //else if (newDir == 9)
-                //{
-                //    delayDirections.Add(DelayDirection.UP_RIGHT);
-                //}
-                //else if (newDir == 10)
-                //{
-                //    delayDirections.Add(DelayDirection.DOWN_RIGHT);
-                //}
-                //else
-                //{
-                //    delayDirections.Add(DelayDirection.IDLE);
-                //}
-
-                Vector3 moveDirection = new Vector3(0.0f, 0.0f, 0.0f);
-                //Vector3 moveDirection2 = new Vector3(0.0f, 0.0f, 0.0f);
-
-                if (Input.GetKey(up_k))
-                {
-                    moveDirection.y -= 1.0f;
-                }
-                if (Input.GetKey(down_k))
-                {
-                    moveDirection.y += 1.0f;
-                }
-                if (Input.GetKey(left_k))
-                {
-                    moveDirection.x -= 1.0f;
-                }
-                if (Input.GetKey(right_k))
-                {
-                    moveDirection.x += 1.0f;
-                }
-                moveDirection.Normalize();
-
-                transform.Translate(moveDirection * speed);
-                //charactersTrans[2].Translate(-moveDirection * speed);
-
-                if (transform.position.x < xMin)
-                {
-                    Vector3 newPos = transform.position;
-                    newPos.x = xMin;
-                    transform.position = newPos;
-                    //charactersTrans[2].Translate(moveDirection.x * speed, 0.0f, 0.0f);
-                }
-                else if (transform.position.x > xMax)
-                {
-                    Vector3 newPos = transform.position;
-                    newPos.x = xMax;
-                    transform.position = newPos;
-                    //charactersTrans[2].Translate(moveDirection.x * speed, 0.0f, 0.0f);
-                }
-                if (transform.position.y < yMin)
-                {
-                    Vector3 newPos = transform.position;
-                    newPos.y = yMin;
-                    transform.position = newPos;
-                    //charactersTrans[2].Translate(0.0f, moveDirection.y * speed, 0.0f);
-                }
-                else if (transform.position.y > yMax)
-                {
-                    Vector3 newPos = transform.position;
-                    newPos.y = yMax;
-                    transform.position = newPos;
-                    //charactersTrans[2].Translate(0.0f, moveDirection.y * speed, 0.0f);
-                }
-
-                //switch (delayDirections[0])
-                //{
-                //    case DelayDirection.UP:
-                //        moveDirection2.y = 1.0f;
-                //        break;
-                //    case DelayDirection.UP_RIGHT:
-                //        moveDirection2.x = 1.0f;
-                //        moveDirection2.y = 1.0f;
-                //        break;
-                //    case DelayDirection.RIGHT:
-                //        moveDirection2.x = 1.0f;
-                //        break;
-                //    case DelayDirection.DOWN_RIGHT:
-                //        moveDirection2.x = 1.0f;
-                //        moveDirection2.y = -1.0f;
-                //        break;
-                //    case DelayDirection.DOWN:
-                //        moveDirection2.y = -1.0f;
-                //        break;
-                //    case DelayDirection.DOWN_LEFT:
-                //        moveDirection2.x = -1.0f;
-                //        moveDirection2.y = -1.0f;
-                //        break;
-                //    case DelayDirection.LEFT:
-                //        moveDirection2.x = -1.0f;
-                //        break;
-                //    case DelayDirection.UP_LEFT:
-                //        moveDirection2.x = -1.0f;
-                //        moveDirection2.y = 1.0f;
-                //        break;
-                //    default:
-                //        break;
-                //}
-                //moveDirection2.Normalize();
-
-                //charactersTrans[2].Translate(moveDirection2 * speed);
-
-                //if (charactersTrans[2].position.x < xMin - 0.5f)
-                //{
-                //    Vector3 newPos = charactersTrans[2].position;
-                //    newPos.x = xMin - 0.5f;
-                //    charactersTrans[2].position = newPos;
-                //}
-                //else if (charactersTrans[2].position.x > xMax - 0.5f)
-                //{
-                //    Vector3 newPos = charactersTrans[2].position;
-                //    newPos.x = xMax - 0.5f;
-                //    charactersTrans[2].position = newPos;
-                //}
-                //if (charactersTrans[2].position.y < yMin + 0.2f)
-                //{
-                //    Vector3 newPos = charactersTrans[2].position;
-                //    newPos.y = yMin + 0.2f;
-                //    charactersTrans[2].position = newPos;
-                //}
-                //else if (charactersTrans[2].position.y > yMax + 0.2f)
-                //{
-                //    Vector3 newPos = charactersTrans[2].position;
-                //    newPos.y = yMax + 0.2f;
-                //    charactersTrans[2].position = newPos;
-                //}
-            //}
+            if (transform.position.x < xMin)
+            {
+                Vector3 newPos = transform.position;
+                newPos.x = xMin;
+                transform.position = newPos;
+            }
+            else if (transform.position.x > xMax)
+            {
+                Vector3 newPos = transform.position;
+                newPos.x = xMax;
+                transform.position = newPos;
+            }
+            if (transform.position.y < yMin)
+            {
+                Vector3 newPos = transform.position;
+                newPos.y = yMin;
+                transform.position = newPos;
+            }
+            else if (transform.position.y > yMax)
+            {
+                Vector3 newPos = transform.position;
+                newPos.y = yMax;
+                transform.position = newPos;
+            }
         }
         else if (curCharacter == Character.SEEING)
         {
-            //if (curMoveDelay >= moveDelay)
-            //{
-            //    delayDirections.Add(DelayDirection.IDLE);
-            //}
-
             if (switchCharacter)
             {
-                //Vector3 direction = new Vector3(-0.5f, 0.2f, 0.0f) - transform.localPosition;
-                //Vector3 direction2 = -charactersTrans[2].localPosition;
-
-                //if (transform.position.x < -0.51f ||
-                //    transform.position.x > -0.49f ||
-                //    transform.position.y < 0.19f ||
-                //    transform.position.y > 0.21f)
-                //{
-                //    if (direction.magnitude > (direction.normalized * switchSpeed).magnitude)
-                //        transform.Translate(direction.normalized * switchSpeed);
-                //    else
-                //    {
-                //        transform.Translate(direction);
-                //        switchCharacter = false;
-                //    }
-                //}
-
-                //if (charactersTrans[2].position.x < -0.01f ||
-                //    charactersTrans[2].position.x > 0.01f ||
-                //    charactersTrans[2].position.y < -0.01f ||
-                //    charactersTrans[2].position.y > 0.01f)
-                //{
-                //    if (direction2.magnitude > (direction2.normalized * switchSpeed).magnitude)
-                //        charactersTrans[2].Translate(direction2.normalized * switchSpeed);
-                //    else
-                //        charactersTrans[2].Translate(direction2);
-                //}
+                spriteAnimator.SetBool("DoSwitch", false);
 
                 switchCharacter = false;
             }
@@ -331,6 +122,7 @@ public class player_scr : MonoBehaviour
                 curCharacter = Character.MOVING;
             }
             switchCharacter = true;
+            spriteAnimator.SetBool("DoSwitch", true);
         }
     }
 
